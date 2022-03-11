4558f051b8353756d3cc1a5e5c674402555dbc9b
Rahul <rahuljayaraman8@gmail.com>
2022-03-11 14:08:15 +0530

    changes in the homescreen

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Cards.css'
 
 const Cards = () => {
+    const [isEdit, setIsEdit] = useState(false)
     return (
         <div className="card text-center mx-auto mt-5">
             <div className="overflow">
@@ -14,10 +15,22 @@ const Cards = () => {
                 <p className="card-text text-secondary">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quam maiores deleniti molestiae culpa consequatur doloribus optio exercitationem repellendus non. Commodi aperiam consequatur asperiores expedita aut et, dolorum illo ut.
                 </p>
-                <a href="#" className='btn btn-outline-primary mx-2'>Edit</a>
-                <a href="#" className='btn btn-outline-danger'>Delete</a>
+                {isEdit && <div>
+                    <textarea type="text" placeholder='Enter message' />
+                </div>}
+                {isEdit ? <button className='btn btn-outline-primary mx-2'
+                >Save</button>
+                    :
+                    <button button className='btn btn-outline-primary mx-2'
+                        onClick={() => setIsEdit(true)}
+                    >Edit</button>}
+                {isEdit ? <button className='btn btn-outline-danger'
+                    onClick={() => setIsEdit(false)}
+
+                >Cancel</button> :
+                    <button className='btn btn-outline-danger'>Delete</button>}
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -7,11 +7,12 @@ const HomeScreen = () => {
             <div className="row">
                 <div className='row my-2'>
                     <div className='d-flex justify-content-center'>
-                        <a href="#" className='btn btn-outline-success mx-2'>+ Add</a>
+                        <textarea type="text" placeholder='Enter message' />
+                        <button className='btn btn-outline-success mx-2'>+ Add</button>
                     </div>
                 </div>
                 {[1, 2, 3, 4, 5].map(() => (
-                    <div className="col-md-12">
+                    <div className="col-md-12" key={Math.random()}>
                         <Cards />
                     </div>
                 ))
